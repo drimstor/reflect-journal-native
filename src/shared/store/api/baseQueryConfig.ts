e170d151a91a6b75e55eda7eea39ffe977a331a7
Vyacheslav Gurin <70888287+drimstor@users.noreply.github.com>
2025-06-04 19:28:56 +0700

    Фикс изменений

--- src/shared/store/api/baseQueryConfig.ts
+++ src/shared/store/api/baseQueryConfig.ts
@@ -1,12 +1,18 @@
-import { tokenService } from "@/src/shared/store";
+import { tokenService, useI18nStore } from "@/src/shared/store";
 import { API_URL } from "../../const";
 
 // Базовая конфигурация для всех запросов
 export const baseQueryConfig = {
   baseUrl: API_URL,
   prepareHeaders: async (headers: Headers) => {
+    // Добавляем токен в заголовки
     const token = await tokenService.getAccessToken();
     if (token) headers.set("Authorization", `Bearer ${token}`);
+
+    // Добавляем язык в заголовки
+    const locale = useI18nStore.getState().locale;
+    headers.set("Accept-Language", locale);
+
     return headers;
   },
 };
